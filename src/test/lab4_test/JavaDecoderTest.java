3bd6d7dbee90bed7597356ca6707e6b3eb3533f8
sashakhlivytska <113299788+sashakhlivytska@users.noreply.github.com>
2024-01-09 01:54:08 +0300

    Update JavaDecoderTest.java

--- src/test/lab4_test/JavaDecoderTest.java
+++ src/test/lab4_test/JavaDecoderTest.java
@@ -1,26 +1,32 @@
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaDecoderTest {
-
     @ParameterizedTest
     @ValueSource(strings = {"t2st", "epdofef", "t2xt", "xivj", "b4th", "nevjoft"})
-    void isEncoded_ShouldReturnTrueForEncodedWord(String encodedWord) {
-        assertTrue(JavaDecoder.isEncoded(encodedWord));
+    void IsEncoded_EncodedWord_ShouldReturnTrue(String decodedWord) {
+        var actual = JavaDecoder.isEncoded(decodedWord);
+
+        assertTrue(actual);
     }
 
     @ParameterizedTest
     @CsvSource({"t2st,test", "616,yay", "f44,foo", "123456,aeiouy"})
-    void decode_VowelEncodedWord_ShouldReturnDecoded(String encodedWord, String expected) {
-        assertEquals(expected, JavaDecoder.decode(encodedWord));
+    public void Decode_EncodedWordWithVowelMethod_ShouldReturnDecoded(String vowelEncodedWord, String expected) {
+        var actual = JavaDecoder.decode(vowelEncodedWord);
+
+        assertEquals(expected, actual);
     }
 
     @ParameterizedTest
     @CsvSource({"vetv,test", "yay,yay", "goo,foo", "cdfghjklmnpqrstvwxzb,bcdfghjklmnpqrstvwxz"})
-    void decode_ConsonantEncodedWord_ShouldReturnDecoded(String encodedWord, String expected) {
-        assertEquals(expected, JavaDecoder.decode(encodedWord));
+    public void Decode_EncodedWordWithConsonantMethod_ShouldReturnDecoded(String consonantEncodedWord, String expected) {
+        var actual = JavaDecoder.decode(consonantEncodedWord);
+
+        assertEquals(expected, actual);
     }
 
     @ParameterizedTest
@@ -29,7 +35,10 @@ class JavaDecoderTest {
             "tone epdofef vezv xivj nevjof gos doptopapvt,some encoded text with method for consonants",
             "s4m2 epdofef t2xt xivj b4th nevjoft,some encoded text with both methods"
     })
-    void decodeText_ShouldReturnDecodedText(String encodedText, String expected) {
-        assertEquals(expected, JavaDecoder.decodeText(encodedText));
+    public void DecodeText_EncodedWordWithVowelMethod_ShouldReturnDecoded(String encodedText, String expected) {
+        var actual = JavaDecoder.decodeText(encodedText);
+
+        assertEquals(expected, actual);
     }
+
 }
